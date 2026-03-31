6f630db1696f22d19d07b164e839ed49910a899e
DukeTran7 <trandoubled.dhk19@st.haui.edu.vn>
2026-03-31 17:04:22 +0700

    change

---
+++ sorting/selectionSort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+    
+void nhapMang(int a[], int n){
+    for (int i=0;i <n; i++){
+        cout << "a[" << i << "] = ";
+        cin >> a[i];
+    }
+}
+
+void xuatMang(int a[], int n){
+    for(int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+void selectionSort(int a[], int n){
+    for(int i = 0; i < n-1; i++){
+        int m=i;
+        for(int j = i+1; j < n; j++){
+            if(a[j] < a[m]){
+                m = j;
+            }
+        }
+        swap(a[i], a[m]);
+    }
+}
+
+int main() {
+    int n;
+    do{
+        cout << "Nhap n: ";
+        cin >> n;
+    }while(n<=0 || n>=100);
+
+    int a[100];
+    nhapMang(a,n);
+    selectionSort(a,n);
+    xuatMang(a,n);
+    return 0;
+}
+
